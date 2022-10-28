ni/openembedded-core

commit 8404b1ac0f2582a38b388c1609cb605f40b962ed

package_manager.py: Change deploy_dir when BUILD_IMAGES_FROM_FEEDS is enabled
When BUILD_IMAGES_FROM_FEEDS is enabled, rootfs' package_write_ipk
dependencies are deleted. Creating a separate package directory with
only the package_write_ipk dependencies does not work in this case.
Revert to the old behavior where deploy_dir is the same as
DEPLOY_DIR_IPK.

Signed-off-by: Shruthi Ravichandran <shruthi.ravichandran@ni.com>

Rebased-by: Alex Stewart <alex.stewart@ni.com>
* Moved original patch changes from package_manager.py to their
  equivalent location in the newer package_manager/ipk module.

meta/lib/oe/package_manager/ipk/__init__.py
@@ -169,11 +169,20 @@ class OpkgPM(OpkgDpkgPM):
         self.pkg_archs = archs
         self.task_name = task_name
 
+
+        if d.getVar('BUILD_IMAGES_FROM_FEEDS'):
+            self.deploy_dir = self.d.getVar("DEPLOY_DIR_IPK")
+        else:
+            self.deploy_dir = oe.path.join(self.d.getVar('WORKDIR'), ipk_repo_workdir)
         self.deploy_dir = oe.path.join(self.d.getVar('WORKDIR'), ipk_repo_workdir)
         self.deploy_lock_file = os.path.join(self.deploy_dir, "deploy.lock")
         self.opkg_cmd = bb.utils.which(os.getenv('PATH'), "opkg")
         self.opkg_args = "--volatile-cache -f %s -t %s -o %s " % (self.config_file, self.d.expand('${T}/ipktemp/') ,target_rootfs)
         self.opkg_args += self.d.getVar("OPKG_ARGS")
+
+        if not d.getVar('BUILD_IMAGES_FROM_FEEDS'):
+            if prepare_index:
+                create_packages_dir(self.d, self.deploy_dir, d.getVar("DEPLOY_DIR_IPK"), "package_write_ipk", filterbydependencies)
 
         if prepare_index:
             create_packages_dir(self.d, self.deploy_dir, d.getVar("DEPLOY_DIR_IPK"), "package_write_ipk", filterbydependencies)

commit e854391366e9a2f23dbff112fa6a16ee78d55af3

fixup! package_manager: Fix erroneous assignments
When BUILD_IMAGES_FROM_FEEDS is enabled, the package manager's
deploy_dir should be set to DEPLOY_DIR_IPK, and directory creation
should be disabled. During the hardknott rebase, commit 8404b1a
accidentally overwrote these assumptions.

Signed-off-by: Shruthi Ravichandran <shruthi.ravichandran@ni.com>

meta/lib/oe/package_manager/ipk/__init__.py
@@ -168,7 +168,6 @@ class OpkgPM(OpkgDpkgPM):
             self.deploy_dir = self.d.getVar("DEPLOY_DIR_IPK")
         else:
             self.deploy_dir = oe.path.join(self.d.getVar('WORKDIR'), ipk_repo_workdir)
-        self.deploy_dir = oe.path.join(self.d.getVar('WORKDIR'), ipk_repo_workdir)
         self.deploy_lock_file = os.path.join(self.deploy_dir, "deploy.lock")
         self.opkg_cmd = bb.utils.which(os.getenv('PATH'), "opkg")
         self.opkg_args = "--volatile-cache -f %s -t %s -o %s " % (self.config_file, self.d.expand('${T}/ipktemp/') ,target_rootfs)
@@ -177,9 +176,6 @@ class OpkgPM(OpkgDpkgPM):
         if not d.getVar('BUILD_IMAGES_FROM_FEEDS'):
             if prepare_index:
                 create_packages_dir(self.d, self.deploy_dir, d.getVar("DEPLOY_DIR_IPK"), "package_write_ipk", filterbydependencies)
-
-        if prepare_index:
-            create_packages_dir(self.d, self.deploy_dir, d.getVar("DEPLOY_DIR_IPK"), "package_write_ipk", filterbydependencies)
 
         self.opkg_dir = oe.path.join(target_rootfs, self.d.getVar('OPKGLIBDIR'), "opkg")
         bb.utils.mkdirhier(self.opkg_dir)